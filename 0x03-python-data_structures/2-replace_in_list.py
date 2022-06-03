fc6037a0d0debaceab75c18b3b250d80f94c9f61
root <root@localhost>
2022-06-03 09:10:08 -0700

    2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    else:
+        length = len(my_list)
+        if idx > length:
+            return my_list
+        else:
+            my_list[idx] = element
+            return my_list
