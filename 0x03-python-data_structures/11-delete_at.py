642007c8a714c26ad67a6e1c7789694b2d6d51a6
root <root@localhost>
2022-06-06 15:41:10 -0700

    Update-1: 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,9 +3,8 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 | idx > len(my_list):
         return my_list
-    else:
-        new_list = my_list[:idx]
-        for i in my_list[idx + 1:]:
-            new_list.append(i)
-        my_list = new_list
-        return my_list
+    new_list = my_list[:idx]
+    for i in my_list[idx + 1:]:
+        new_list.append(i)
+    my_list = new_list
+    return my_list
