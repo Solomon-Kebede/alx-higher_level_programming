a341a6c8d2a7cf52ae5e7d39e86befbc062a5e70
root <root@localhost>
2022-07-07 08:43:06 -0700

    Update#1: 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     '''Reads a text file and prints contents to stdout'''
     with open(filename, 'r', encoding="utf-8") as f:
-        content = f.read()
-    print(content)
+        for line in f:
+            print(line, end='')
