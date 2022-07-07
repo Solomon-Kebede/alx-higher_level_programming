1b761d15a35dc1a79cae2923a1ea2312729f1f81
root <root@localhost>
2022-07-07 08:17:40 -0700

    Document module and function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Module to read a file"""
+
 
 def read_file(filename=""):
+    '''Reads a text file and prints contents to stdout'''
     with open(filename, 'r', encoding="utf-8") as f:
         content = f.read()
     print(content)
