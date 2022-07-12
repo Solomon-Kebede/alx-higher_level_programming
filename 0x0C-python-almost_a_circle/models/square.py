0fa3624046ce691941ab98840384b4836df8df20
root <root@localhost>
2022-07-11 17:39:51 -0700

    11. Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,4 +9,26 @@ class Square(Rectangle):
     """Class documentation"""
     def __init__(self, size, x=0, y=0, id=None):
         '''Function documentation'''
-        super().__init__(size, size, x, y, id)
+        self.size = size
+        super().__init__(self.size, self.size, x, y, id)
+
+    @property
+    def size(self):
+        """size getter method"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter method"""
+        if isinstance(value, int) and value > 0:
+            self.__size = value
+        elif not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+
+    def __str__(self):
+        """override the method to display rectangle info"""
+        x = self.x
+        y = self.y
+        return f"[Square] ({self.id}) {x}/{y} - {self.size}"
