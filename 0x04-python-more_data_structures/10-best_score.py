5456017d676716d52b848e9a78fa00460d77dac8
root <root@localhost>
2022-09-05 09:23:08 -0700

    0x12-javascript-warm_up

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if isinstance(a_dictionary, dict):
-        if a_dictionary == {}:
-            return None
-        else:
-            return sorted(list(a_dictionary.items()))[-1][0]
-    else:
+    if not isinstance(a_dictionary, dict) or a_dictionary == dict():
         return None
+    return sorted(list(a_dictionary.items()))[-1][0]
