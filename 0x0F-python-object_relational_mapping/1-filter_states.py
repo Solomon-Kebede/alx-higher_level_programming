6074e13af35df04955cd3891d5eb6fefe4f87454
root <root@localhost>
2022-09-11 13:13:12 -0700

    Task 1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+'''
+Write a script that lists all `states` with a `name` starting
+with `N` (upper N) from the database `hbtn_0e_0_usa`:
+
+    -Your script should take 3 arguments: `mysql username`, `mysql password`
+    and `database name` (no argument validation needed)
+    -You must use the module `MySQLdb (import MySQLdb)`
+    -Your script should connect to a MySQL server running
+    on `localhost` at port `3306`
+    -Results must be sorted in ascending order by `states.id`
+    -Your code should not be executed when imported
+'''
+
+
+from sys import argv
+import MySQLdb
+
+
+def list_states_starting_with_N(user, passwd, db):
+    '''List all states ordered by id in
+    ascending order that start with "N"'''
+    db = MySQLdb.connect(
+        host="localhost",
+        user=user,
+        passwd=passwd,
+        db=db,
+        port=3306)
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states ORDER BY states.id ASC;')
+    for result in cursor.fetchall():
+        if result[1].startswith('N'):
+            print(result)
+
+
+if __name__ == '__main__':
+    list_states_starting_with_N(argv[1], argv[2], argv[3])
