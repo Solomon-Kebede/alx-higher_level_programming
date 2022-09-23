6dcf4b2263a1f386b23bbd47ef39843722c0fb2a
root <root@localhost>
2022-09-22 17:54:33 -0700

    Update-2: Task 1. cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script takes in a URL, sends a `GET` request to the URL, and displays the body of the response if `200` status code response
-if [ $(curl -sI $1 | grep '200 OK' -c ) -eq 1 ]; then curl $1; fi
+curl -sL $1
