d836583a74fbc8c5e7aa6b6a2c6aa5ade79a642e
root <root@localhost>
2022-09-22 17:48:44 -0700

    Update-1: Task 1. cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Write a Bash script that takes in a URL, sends a `GET` request to the URL, and displays the body of the response
-#     Display only body of a `200` status code response
-#     You have to use `curl`
-# Please test your script in the sandbox provided, using the web server running on port 5000
-if [ $(curl -sI $1 | grep '200 OK' -c ) -eq 1 ]
-then
-	curl $1
-fi
+# script takes in a URL, sends a `GET` request to the URL, and displays the body of the response if `200` status code response
+if [ $(curl -sI $1 | grep '200 OK' -c ) -eq 1 ]; then curl $1; fi
