33640d7bcd6364e2f608e831cad7823e694bb6a7
Solomon Kebede <solomonaau188@gmail.com>
2022-10-26 23:18:57 +0000

    Task 3. Add `.red` class

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,12 @@
+/*
+Write a JavaScript script that adds the class `red` to the `<header>` element when the user clicks on the tag `DIV#red_header`
+  -You can’t use `document.querySelector` to select the HTML tag
+  -You must use the JQuery API
+*/
+
+const header = $('header');
+const redHeader = $('div#red_header');
+
+redHeader.click(function () {
+  header.addClass('red');
+});
