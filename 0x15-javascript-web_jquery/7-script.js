361bb34497af9a40778f4ce3ad09a0ac246b9470
Solomon Kebede <solomonaau188@gmail.com>
2022-10-27 00:59:53 +0000

    Task 7. Star wars character

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,13 @@
+/*
+Write a JavaScript script that fetches the character `name` from this URL: `https://swapi-api.hbtn.io/api/people/5/?format=json`
+  -The name must be displayed in the HTML tag `DIV#character`
+  -You can’t use `document.querySelector` to select the HTML tag
+  -You must use the JQuery API
+*/
+
+const apiUrl = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+const character = $('div#character');
+const res = $.getJSON(apiUrl, function () {
+  const name = res.responseJSON.name;
+  character.text(name);
+});
