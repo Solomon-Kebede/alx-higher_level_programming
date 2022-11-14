6b3456bb9e026ec718402a6c586d1e2b50fe7ae4
Solomon Kebede <solomonaau188@gmail.com>
2022-11-14 16:43:59 +0000

    Task 2. Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+-Write a function that returns True if the object is
+exactly an instance of the specified class ; otherwise False.
+    -Prototype: def is_same_class(obj, a_class):
+    -You are not allowed to import any module
+'''
+
+
+def is_same_class(obj, a_class):
+    '''Returns True if the object is
+    exactly an instance of the specified class;
+    otherwise False'''
+    return type(obj) is a_class
