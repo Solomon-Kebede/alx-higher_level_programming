a667b26b5c5be8081209311f5f4e2f60c49954fd
Solomon Kebede <solomonaau188@gmail.com>
2022-11-14 18:24:26 +0000

    [Task 3] Pycodestyle fix

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 '''
--Write a function that returns `True` if the object is an 
+-Write a function that returns `True` if the object is an
 instance of, or if the object is an instance of a class
 that inherited from, the specified class ; otherwise `False`.
     -Prototype: `def is_kind_of_class(obj, a_class):`
