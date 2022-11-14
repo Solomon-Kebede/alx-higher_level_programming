36c325b46c2e1452d1be9dee9a64723ee3e25f90
Solomon Kebede <solomonaau188@gmail.com>
2022-11-14 18:18:02 +0000

    [Task 2] Comment update

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 '''
--Write a function that returns True if the object is
-exactly an instance of the specified class ; otherwise False.
-    -Prototype: def is_same_class(obj, a_class):
+-Write a function that returns `True` if the object is
+exactly an instance of the specified class ; otherwise `False`.
+    -Prototype: `def is_same_class(obj, a_class)`:
     -You are not allowed to import any module
 '''
 
