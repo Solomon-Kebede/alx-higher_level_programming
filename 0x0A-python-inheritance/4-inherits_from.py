a8d2e047d10f3bf828b7c497da43f06ccf29b15f
Solomon Kebede <solomonaau188@gmail.com>
2022-11-15 07:34:28 +0000

    Task 4. Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+'''
+-Write a function that returns `True` if the object is an instance
+of a class that inherited (directly or indirectly) from the
+specified class ; otherwise `False`.
+    -Prototype: `def inherits_from(obj, a_class):`
+    -You are not allowed to import any module
+
+'''
+
+
+def inherits_from(obj, a_class):
+    '''Returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the
+    specified class; otherwise False'''
+    print(f"{obj}  = {a_class}")
+    return issubclass(type(obj), a_class)
