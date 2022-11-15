15bff6b05581e076484bf6cfb3203cad01a91a3e
Solomon Kebede <solomonaau188@gmail.com>
2022-11-15 07:37:55 +0000

    [Task 4] Remove print statement

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,5 +14,4 @@ def inherits_from(obj, a_class):
     '''Returns True if the object is an instance of a class
     that inherited (directly or indirectly) from the
     specified class; otherwise False'''
-    print(f"{obj}  = {a_class}")
     return issubclass(type(obj), a_class)
